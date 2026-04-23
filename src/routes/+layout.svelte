510becc2abc2cf54c596549c4676d03f40f024d1
Dylan <dylan.hernandez@mail.polimi.it>
2026-04-23 21:07:12 +0200

    styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
-	import"$lib/styles/reset.css";
+	import "$lib/styles/reset.css";
+	import "$lib/styles/tokens/tokens.css";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<link rel="preconnect" href="https://use.typekit.net" />
+	<link rel="stylesheet" href="https://use.typekit.net/ukz2bfk.css" />
 </svelte:head>
 
 {@render children()}
